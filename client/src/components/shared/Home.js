95e3311b83bf4cd8b134f498fd0d2c6bc71c2edd
Sean Schmidt <schmidtsean048@gmail.com>
2021-03-26 17:46:19 -0600

    typo

--- client/src/components/shared/Home.js
+++ client/src/components/shared/Home.js
@@ -3,7 +3,7 @@ const Home = () => {
 return(
   <>
   <HomeTitle> Welcome to Devpoint Handymen</HomeTitle>
-  <HomeBody>At Devpoint Handymen, we are here to give you with the best service possible.
+  <HomeBody>At Devpoint Handymen, we are here to give you the best service possible.
     We garantee that we have the right worker for you.
   </HomeBody>
   <p style={{textAlign:"center"}}>Please browse through our Handymen to select which specialist can help you.</p>
