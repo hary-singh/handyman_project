480e0991fb8c9f1cf2e09cc2c0d492d64b0839b5
Sean Schmidt <schmidtsean048@gmail.com>
2021-03-26 10:30:18 -0600

    NoMatch page added

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { Route, Switch } from 'react-router-dom';
 import Handymen from './components/handymen/Handymen';
 import Home from './components/shared/Home';
 import Navbar from './components/shared/Navbar';
+import NoMatch from './components/shared/NoMatch';
 import { Container } from 'semantic-ui-react';
 
 const App = () => (
@@ -11,6 +12,7 @@ const App = () => (
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/handymen" component={Handymen} />
+          <Route component= {NoMatch} />
         </Switch>
       </Container>
   </>
