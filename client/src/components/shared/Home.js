0a9d6bedfe5361cac6429f1f87332162edb390da
Sean Schmidt <schmidtsean048@gmail.com>
2021-03-26 13:20:45 -0600

    fixed a duplicate text

--- client/src/components/shared/Home.js
+++ client/src/components/shared/Home.js
@@ -4,7 +4,7 @@ return(
   <>
   <HomeTitle> Welcome to Devpoint Handymen</HomeTitle>
   <HomeBody>At Devpoint Handymen, we are here to give you with the best service possible.
-    We garantee that we have the right worker for you. Please browse through our Handymen to select which specialist can help you.
+    We garantee that we have the right worker for you.
   </HomeBody>
   <p style={{textAlign:"center"}}>Please browse through our Handymen to select which specialist can help you.</p>
   </>
