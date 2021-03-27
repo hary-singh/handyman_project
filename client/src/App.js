420626f6e02d5644f431f846da2598fac8306bb0
Harry Dhillon <navbirsdhillon@gmail.com>
2021-03-26 19:45:37 -0600

    sticky navbar

--- client/src/App.js
+++ client/src/App.js
@@ -3,18 +3,20 @@ import Handymen from './components/handymen/Handymen';
 import Home from './components/shared/Home';
 import Navbar from './components/shared/Navbar';
 import NoMatch from './components/shared/NoMatch';
-import { Container } from 'semantic-ui-react';
+import { Container, Sticky } from 'semantic-ui-react';
 
 const App = () => (
   <>
+    <Sticky>
       <Navbar />
-      <Container>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/handymen" component={Handymen} />
-          <Route component= {NoMatch} />
-        </Switch>
-      </Container>
+    </Sticky>
+    <Container>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/handymen" component={Handymen} />
+        <Route component= {NoMatch} />
+      </Switch>
+    </Container>
   </>
 )
 export default App;
